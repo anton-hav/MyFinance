88d82df3745cce6162b3ce5f2757edbbca8363b3
anton-hav <anton.havrilenia@hotmail.com>
2023-01-10 22:55:47 +0100

    adds new migration that add role names for admin and user by default

---
+++ WebApi/MyFinance.DataBase/Migrations/20230110215447_RolesRecordsByDefaultToDataBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFinance.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesRecordsByDefaultToDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    {Guid.NewGuid().ToString("D"), "Admin"},
+                    {Guid.NewGuid().ToString("D"), "User"}
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
