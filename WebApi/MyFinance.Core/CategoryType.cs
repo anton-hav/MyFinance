2dd267d0e3c0c4e613d8ca111eb74acfffdc55da
anton-hav <anton.havrilenia@hotmail.com>
2023-01-15 00:04:03 +0100

    adds new category entity

---
+++ WebApi/MyFinance.Core/CategoryType.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MyFinance.Core;
+
+/// <summary>
+/// Category type.
+/// </summary>
+[JsonConverter(typeof(StringEnumConverter))]
+public enum CategoryType
+{
+    Income = 0,
+    Expenses = 1,
+}
