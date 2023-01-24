6993b50091a5c5fac9e306d3a9ae23bbbae13d45
anton-hav <anton.havrilenia@hotmail.com>
2023-01-24 13:45:07 +0100

    adds new migrations that add PlannedTransactions table to the database

---
+++ WebApi/MyFinance.DataBase/Migrations/20230124123850_AddPlannedTransactionsTableToDatabase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFinance.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlannedTransactionsTableToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlannedTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Crone = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    JobId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlannedTransactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlannedTransactions_CategoryId_Price_Crone",
+                table: "PlannedTransactions",
+                columns: new[] { "CategoryId", "Price", "Crone" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlannedTransactions_JobId",
+                table: "PlannedTransactions",
+                column: "JobId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlannedTransactions");
+        }
+    }
+}
---
+++ WebApi/MyFinance.DataBase/Migrations/20230124124104_AddRelationshipPlannedTransactionAndCategory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFinance.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationshipPlannedTransactionAndCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlannedTransactions_Categories_CategoryId",
+                table: "PlannedTransactions",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlannedTransactions_Categories_CategoryId",
+                table: "PlannedTransactions");
+        }
+    }
+}
