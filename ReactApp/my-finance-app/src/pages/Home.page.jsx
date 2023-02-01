a04ed953504278df8437bed04a39854d484fd2ab
anton-hav <anton.havrilenia@hotmail.com>
2023-02-01 19:37:24 +0100

    adds welcome text to the Home page

--- ReactApp/my-finance-app/src/pages/Home.page.jsx
+++ ReactApp/my-finance-app/src/pages/Home.page.jsx
@@ -1,7 +1,12 @@
+// Import third-party libraries
+import { Paper, Box, Typography } from "../imports/ui.imports";
+import { Link } from "../imports/navigation.imports";
 // Import services
 import UserService from "../services/user.service";
 // Import custom types and utils
 import useToken from "../utils/hooks/useToken.hook";
+// Import styles
+import "./home.page.css";
 
 const _userService = new UserService();
 
@@ -13,5 +18,30 @@ export default function HomePage() {
     validateToken();
   }
 
-  return <div>Welcome</div>;
+  return (
+    <Box className="home">
+      <Paper className="wrapper">
+        <Typography variant="h1">Welcom.</Typography>
+        <Box className="summary">
+          <Typography mb={1}>
+            This app allows you to manage and analyze your expenses and income.
+          </Typography>
+          <Typography mb={2}>
+            The application provides the following features:
+            <br />
+            - create and manage transaction records;
+            <br />
+            - use default categories;
+            <br />
+            - create and manage new categories,
+            <br />- create and manage scheduled transactions.
+          </Typography>
+          <Typography>
+            To start using the service, <Link to="/login">log in</Link> or{" "}
+            <Link to="/register">register</Link> a new account.
+          </Typography>
+        </Box>
+      </Paper>
+    </Box>
+  );
 }
